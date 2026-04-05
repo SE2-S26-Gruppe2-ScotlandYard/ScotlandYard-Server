9edbcf77e8c4a88bb2c0f119103bf3d51dd0ac56
Maverick Haas <191795490+mav42@users.noreply.github.com>
2026-04-05 15:19:35 +0200

    feature add GameState class to save the game

---
+++ src/main/java/at/aau/serg/websocketdemoserver/gamelogic/GameState.java
@@ -0,0 +1,19 @@
+package at.aau.serg.websocketdemoserver.gamelogic;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import at.aau.serg.websocketdemoserver.gamelogic.board.Board;
+import at.aau.serg.websocketdemoserver.gamelogic.player.Player;
+
+public class GameState {
+    private final String gameId;
+    private final Board board;
+    private final Map<String, Player> players = new HashMap<>();
+    protected Map<String, Integer> playerPositions = new HashMap<>();
+
+    public GameState (String gameId) {
+        this.gameId = gameId;
+
+    }
+}
