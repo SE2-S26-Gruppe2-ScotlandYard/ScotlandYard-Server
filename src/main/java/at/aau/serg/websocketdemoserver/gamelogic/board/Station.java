f961988777d7a7878e5d0b4474557932910017d7
Maverick Haas <191795490+mav42@users.noreply.github.com>
2026-04-05 18:35:21 +0200

    refactor use Lombok Getter in Station

--- src/main/java/at/aau/serg/websocketdemoserver/gamelogic/board/Station.java
+++ src/main/java/at/aau/serg/websocketdemoserver/gamelogic/board/Station.java
@@ -3,17 +3,16 @@ package at.aau.serg.websocketdemoserver.gamelogic.board;
 import java.util.Collections;
 import java.util.List;
 
+import lombok.Getter;
+
 public class Station {
+    @Getter
     private final int number;
     private final List<Connection> connections;
 
     public Station(int number, List<Connection> connections) {
         this.number = number;
         this.connections = connections;
-    }
-
-    public int getNumber() {
-        return number;
     }
 
     public List<Connection> getConnections() {
