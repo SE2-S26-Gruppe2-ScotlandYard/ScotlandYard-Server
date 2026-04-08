31f3c4e04ff3bd7b9d869a580754194c8820b52a
Maverick Haas <191795490+mav42@users.noreply.github.com>
2026-04-08 16:59:06 +0200

    add Sonar ignor tag to @Data

--- src/main/java/at/aau/serg/websocketdemoserver/dtos/movement/MovementData.java
+++ src/main/java/at/aau/serg/websocketdemoserver/dtos/movement/MovementData.java
@@ -5,7 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data
+@Data   //NOSONAR
 @AllArgsConstructor
 @NoArgsConstructor
 public class MovementData {
