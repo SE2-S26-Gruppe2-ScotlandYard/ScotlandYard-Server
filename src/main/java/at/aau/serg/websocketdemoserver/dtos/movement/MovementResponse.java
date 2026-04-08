73391c0659a86b7b4b96611bc9f3f7549355a169
Maverick Haas <191795490+mav42@users.noreply.github.com>
2026-04-08 15:42:13 +0200

    docs/feature add tests for the dtos

--- src/main/java/at/aau/serg/websocketdemoserver/dtos/movement/MovementResponse.java
+++ src/main/java/at/aau/serg/websocketdemoserver/dtos/movement/MovementResponse.java
@@ -2,9 +2,11 @@ package at.aau.serg.websocketdemoserver.dtos.movement;
 
 import lombok.Data;
 import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class MovementResponse {
     private boolean success;
     private String message;
