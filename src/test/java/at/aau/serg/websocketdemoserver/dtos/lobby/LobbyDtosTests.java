e52e4f648b79985d5a3dcb2464fbe9acd80b1715
Stefan Sodat <stefan@MacBook-Air-von-Stefan.local>
2026-04-10 10:15:55 +0200

    increase lobby dto coverage

--- src/test/java/at/aau/serg/websocketdemoserver/dtos/lobby/LobbyDtosTests.java
+++ src/test/java/at/aau/serg/websocketdemoserver/dtos/lobby/LobbyDtosTests.java
@@ -24,6 +24,16 @@ public class LobbyDtosTests {
     }
 
     @Test
+    void testCreateLobbyMessageAllArgsConstructor() {
+        CreateLobbyMessage message = new CreateLobbyMessage("TestLobby", "1", "Stefan", "pass");
+
+        assertEquals("TestLobby", message.getLobbyName());
+        assertEquals("1", message.getUserId());
+        assertEquals("Stefan", message.getUserName());
+        assertEquals("pass", message.getPassword());
+    }
+
+    @Test
     void testJoinLobbyMessageNoArgsConstructorAndSetters() {
         JoinLobbyMessage message = new JoinLobbyMessage();
 
@@ -31,6 +41,16 @@ public class LobbyDtosTests {
         message.setUserId("2");
         message.setUserName("Player");
         message.setPassword("secret");
+
+        assertEquals("lobby-123", message.getLobbyId());
+        assertEquals("2", message.getUserId());
+        assertEquals("Player", message.getUserName());
+        assertEquals("secret", message.getPassword());
+    }
+
+    @Test
+    void testJoinLobbyMessageAllArgsConstructor() {
+        JoinLobbyMessage message = new JoinLobbyMessage("lobby-123", "2", "Player", "secret");
 
         assertEquals("lobby-123", message.getLobbyId());
         assertEquals("2", message.getUserId());
@@ -50,11 +70,27 @@ public class LobbyDtosTests {
     }
 
     @Test
+    void testLeaveLobbyMessageAllArgsConstructor() {
+        LeaveLobbyMessage message = new LeaveLobbyMessage("lobby-456", "3");
+
+        assertEquals("lobby-456", message.getLobbyId());
+        assertEquals("3", message.getUserId());
+    }
+
+    @Test
     void testDeleteLobbyMessageNoArgsConstructorAndSetters() {
         DeleteLobbyMessage message = new DeleteLobbyMessage();
 
         message.setLobbyId("lobby-789");
         message.setRequesterId("host-1");
+
+        assertEquals("lobby-789", message.getLobbyId());
+        assertEquals("host-1", message.getRequesterId());
+    }
+
+    @Test
+    void testDeleteLobbyMessageAllArgsConstructor() {
+        DeleteLobbyMessage message = new DeleteLobbyMessage("lobby-789", "host-1");
 
         assertEquals("lobby-789", message.getLobbyId());
         assertEquals("host-1", message.getRequesterId());
@@ -76,6 +112,21 @@ public class LobbyDtosTests {
         assertEquals("Lobby created successfully", response.getMessage());
         assertEquals(lobby.getId(), response.getLobbyId());
         assertEquals(lobby, response.getLobby());
+    }
+
+    @Test
+    void testLobbyResponseNoArgsConstructorAndSetters() {
+        LobbyResponse response = new LobbyResponse();
+
+        response.setSuccess(true);
+        response.setMessage("Lobby created successfully");
+        response.setLobbyId("lobby-123");
+        response.setLobby(null);
+
+        assertTrue(response.isSuccess());
+        assertEquals("Lobby created successfully", response.getMessage());
+        assertEquals("lobby-123", response.getLobbyId());
+        assertNull(response.getLobby());
     }
 
     @Test
